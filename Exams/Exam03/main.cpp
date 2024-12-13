ea1bf159210f56411c3b4141421b4229bd4061a4
Cortney M <123661123+Lythos0@users.noreply.github.com>
2024-12-13 18:47:27 -0500

    Update main.cpp

--- Exams/Exam03/main.cpp
+++ Exams/Exam03/main.cpp
@@ -1,6 +1,143 @@
 #include "Utils.h"
-
 //Define Classes Below Here
+class BalanceManager: public Object{
+
+private:
+float _amount;
+int _count = 0;
+public:
+BalanceManager(){
+	_amount = 0;
+	_count++;}
+
+BalanceManager(float _newamount){
+	_amount = _newamount;
+	_count++;
+	if(_amount < 0){
+		_amount =0;}
+}
+virtual bool possible_deposit(float money){
+	return True
+}
+
+virtual bool poosible_withdraw(float money){
+	return True
+		}
+
+virtual bool deposit(float money){
+	_amount +=money;
+	if(money ! < 0 && money != possible_deposit){
+		return true;
+			
+	else {
+		return false;
+	}
+}
+}
+
+virtual bool withdraw(float money){
+	_amount -= money;
+	if(money ! < 0 && money != possible_withdraw){
+		return true; 
+}
+	else {
+		return false;
+	}
+}
+	
+int balance(){
+	return _amount;
+}
+int total_accounts(){
+	return _count;
+}
+std::string to_str() {
+        std::ostringstream oss;
+        oss << "Balance: " << std::fixed << std::setprecision(2) << _amount << " USD";
+        return oss.str();
+    }
+}
+
+};
+
+class BalanceManagerWithDailyTurnOver : public BalanceManager
+private:
+float _current;
+float _maximum;
+public:
+BalanceManagerWithDailyTurnOver(){
+_amount = 0;
+_current = 0;
+_maximum = 5000;
+}
+
+bool possible_transaction(float transaction){
+if(_current + transaction <= _maximum){
+return true; }
+
+else {
+return false;
+}
+}
+
+bool possible_deposit(float transaction) override {
+         return possible_transaction(transaction);  
+    }
+}
+
+bool possible_withdraw(float transaction) override {
+return possible_transaction(transaction);
+	}
+}
+
+
+bool deposit(float transaction) override {
+        if (BalanceManager::deposit(transaction)) {
+            if (possible_transaction(transaction)) {
+                _current += transaction;  
+                return true;  
+            }
+        }
+        return false;  
+    }
+
+   
+    bool withdraw(float transaction) override {
+        
+        if (BalanceManager::withdraw(transaction)) {
+           
+            if (possible_transaction(-transaction)) {
+                _current -= transaction;  
+                return true; 
+            }
+        }
+        return false; 
+    }
+};
+
+float turnover(){
+return _current;
+}
+
+void adjustment(float new_maximum) {
+        if (new_maximum >= _current) {
+            _maximum = new_maximum;
+        }
+    }
+
+    void reset() {
+        _current = 0;
+    }
+
+    std::string to_str() override {
+        std::ostringstream oss;
+        oss << std::fixed << std::setprecision(2);
+        oss << "Balance: " << _amount << " USD\n";
+        oss << "Turnover: " << _current << " USD\n";
+        oss << "Limit: " << _maximum << " USD";
+        return oss.str();
+    }
+};
 
 //Define Classes Above Here 
 
